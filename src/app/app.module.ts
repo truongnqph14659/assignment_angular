52c0b6f1e153af79bbf6e35d5d941aa577dabb2b
truongnqph14659 <77135926+truongnqph14659@users.noreply.github.com>
2022-09-29 22:54:56 +0700

    'trang-chủ-#13'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { HeaderComponent } from './components/admin/header/header.component';
 import { SidebarComponent } from './components/admin/sidebar/sidebar.component';
+import { DetailpageComponent } from './components/client/detailpage/detailpage.component';
+import { ClientComponent } from './components/client/client.component';
+import { FooterComponent } from './components/client/footer/footer.component';
+import { NavbarComponent } from './components/client/navbar/navbar.component';
+import { ProducthomeComponent } from './components/client/producthome/producthome.component';
+import { HomeheaderComponent } from './components/client/homeheader/homeheader.component';
+import { SlideshowComponent } from './components/client/slideshow/slideshow.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    DetailpageComponent,
+    ClientComponent,
+    FooterComponent,
+    NavbarComponent,
+    ProducthomeComponent,
+    HomeheaderComponent,
+    SlideshowComponent
   ],
   imports: [
     BrowserModule,
